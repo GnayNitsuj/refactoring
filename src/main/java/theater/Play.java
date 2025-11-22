4b83fdbfdd4b869419714b858e724697aaa93243
GnayNitsuj <Justin20061113@gmail.com>
2025-11-21 22:19:02 -0500

    Task 1.1 Completed

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,27 @@
 package theater;
 
+/**
+ * Class that represents a play which can be performed.
+ *
+ * <p>This object stores the name and type of the play.
+ * Neither field should ever be {@code null}. Hence, callers also need to provide
+ * non-null values when creating a play.</p>
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
